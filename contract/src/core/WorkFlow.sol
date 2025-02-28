5036e85f1d85e6ab9007fa551f1961c8c8f49bdf
Maa-ly <lydiaahenkorahgyamfi@gmail.com>
2025-02-28 11:05:18 -0800

    workFlow

--- contract/src/core/WorkFlow.sol
+++ contract/src/core/WorkFlow.sol
@@ -3,9 +3,11 @@ pragma solidity 0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./Dao.sol";
 
 contract WorkFlow is ERC20{
 IERC20 vaLit;
+Dao public dao;
 //uint256 amountTomint;
 address owner;
     constructor(uint256 _amountTomint/*, address _owner*/) ERC20("vaLiT", "VT") {
@@ -40,6 +42,7 @@ validators[] _validators;
 
  bool activeValidation;
  bool isvalidator;
+ mapping(address => ValidatorParam => ContributionInfo) public validatorToContributor;
 
     function addValidator(address _validator) external{
       require(_validator != address(0));
@@ -47,7 +50,9 @@ validators[] _validators;
       isvalidator = true;
 
     }
-    function hasBeenValidated(uint256 _contributionHash) public view returns(bool) {}
+    function hasBeenValidated(uint256 _contributionHash) public view returns(bool) {
+      //validatorToContributor[]
+    }
 
 //anyone can remove validator if they have no rewards, cannot remove if theyre actively validating a contribution
     function removeVAlidator(address _validator) external {
@@ -57,14 +62,42 @@ validators[] _validators;
 
 // zktls proof the validator work
 
+
+   function getValidatorreputation (address validatorAddress) public view returns(ValidatorParam memory) {
+      return ValidatorParam.reward , ValidatorParam.numberOFContributorvalidated, ValidatorParam.penalty;
+   }
+
+
+   function validate(string memory _feedback, bool _isVald) external returns(ValidatorParam memory) {
+      if(!isvalidator){
+         revert WorkFlow__MustBeAvalidator;
+      }
+      validationInfo[msg.sender] = ValidatorParam({
+         feedback :_feedback ;
+         isVald :_isVald ;
+         time : block.timestamp;
+         isactiveValidator : true;
+         reward : 0;
+         penalty : 0;
+         numberOFContributorvalidated :  numberOFContributorvalidated + 1;
+
+      })
+
+  return ValidatorParam.isVald, ValidatorParam.feedback;
+   }
+
    function validatePOW() external {}
-
-   function getValidatorreputation () external {}
-
-   function validate() external {}
-
 // swap validator erc for the ld token/ xp token
-   function swap() external {}
+   function swap(uint256 amount, address to) external {
+     ValidatorParam memory info = validationInfo[to];
+       uint256 _amount = info.reward;
+       require(amount <= _amount, "WorkFlow__Not_Enough_Balance");
+     //  dao.
+     //burn validator token and mint xp
+     
+
+
+        }
 
 // compere the valid/ good bad/ invarid <
    function getvalidation() public {
